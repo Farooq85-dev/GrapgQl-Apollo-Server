d08d0a17cc20f72e0522000af6409635587d573b
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-11-25 12:29:12 +0500

    relations added

--- resolvers.js
+++ resolvers.js
@@ -7,9 +7,26 @@ export const resolvers = {
     authors: () => authors,
     singleReview: (parent, { id }) =>
       reviews.find((review) => review.id === id),
-    singleAuthor: (parent, { name }) =>
-      authors.find((author) => author.name === name),
-    singleGame: (parent, { title }) =>
-      games.find((game) => game.title === title),
+    singleAuthor: (parent, { id }) =>
+      authors.find((author) => author.id === id),
+    singleGame: (parent, { id }) => games.find((game) => game.id === id),
+  },
+  Game: {
+    reviews(parent) {
+      return reviews.filter((review) => review.gameId === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return authors.find((authors) => authors.id === parent.author_id);
+    },
+    game(parent) {
+      return games.find((game) => game.id === parent.game_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return reviews.filter((review) => review.authorId === parent.id);
+    },
   },
 };
--- schema.js
+++ schema.js
@@ -2,13 +2,16 @@ export const typeDefs = `#graphql
     type Game {
         id: ID!
         title: String!
-        platforms: [String!]!   
+        platforms: [String!]!
+        reviews: [Review!] # A game can have multiple reviews
     }
 
     type Review {
         id: ID!
         rating: Int!
-        content: String!    
+        content: String!
+        author: Author!  # One review belongs to one author
+        game: Game!      # One review corresponds to one game
     }
 
     type Author {
@@ -16,14 +19,16 @@ export const typeDefs = `#graphql
         username: String!
         name: String!
         verified: Boolean!
+        reviews: [Review!] # An author can write multiple reviews
     }
-    # Entry Point For Querying Reviews, Games and Authors  
+
+    # Entry Point for Querying Reviews, Games, and Authors
     type Query {
         reviews: [Review]
         games: [Game]
         authors: [Author]
-        singleReview(id:ID!):Review
-        singleAuthor(name:String!):Author
-        singleGame(title:String!):Game
+        singleReview(id: ID!): Review
+        singleAuthor(id: ID!): Author
+        singleGame(id: ID!): Game
     }
 `;
